9b32f6e1bb9b07b63bf8dcd1f2155cdf17ffdfe5
MirganiMirkurbonov <110224082+MirganiMirkurbonov@users.noreply.github.com>
2023-04-19 12:58:25 +0500

    some changes

---
+++ Domain/Entities/Common/EntityTrackedWithState.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Common;
+
+internal class EntityTrackedWithState<Tid> : Entity<Tid>
+{
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [Column("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+
+    [Column("state")]
+    public State State{ get; set; }
+}
