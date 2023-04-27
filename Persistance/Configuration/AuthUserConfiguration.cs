99a9fd2f56c7102b0f0c74e43bc3408584f87c1a
MirganiMirkurbonov@Gmail.com <MirganiMirkurbonov@Gmail.com>
2023-04-27 11:40:30 +0500

    some chaneg

--- Persistance/Configuration/AuthUserConfiguration.cs
+++ Persistance/Configuration/AuthUserConfiguration.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities.Auth;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Persistance.Configuration
+namespace Persistance.RootContext;
+
+public class AuthUserConfiguration : IEntityTypeConfiguration<AuthUser>
 {
-    internal class EntityTypeConfiguration
+    public void Configure(EntityTypeBuilder<AuthUser> builder)
     {
+        builder.HasIndex(x => x.Email);
+        builder.HasIndex(x => x.PhoneNumber);
+        builder.HasQueryFilter(x => x.State != Domain.Enums.State.Deleted);
     }
 }
